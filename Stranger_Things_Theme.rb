5af28d1b2a6c02a8fa2fe5105aae5f4829dda798
arrysele <36054143+arrysele@users.noreply.github.com>
2018-02-14 14:29:42 -0800

    Add files via upload

--- Stranger_Things_Theme.rb
+++ Stranger_Things_Theme.rb
@@ -1,29 +1,21 @@
-use_bpm 120
-live_loop :hihat do
-  sample :drum_cymbal_closed, amp:1
-  sleep 1
-end
-live_loop :bass do
-  sync :hihat #this will automatically sync with another loop
-  sample :drum_heavy_kick
-  sleep 2
-end
-live_loop :snare do
-  sync :hihat
-  sleep 1
-  sample :drum_snare_soft, amp: 2
-  sleep 1
-end
-live_loop :tom_mid do
-  sync :hihat
-  sleep 1
-  sample :drum_tom_mid_soft, amp: 2
-  sleep 1
-end
-live_loop :hi_tom do
-  sync :hihat
-  sample :drum_tom_hi_soft, amp: 3
-  sleep 2
-end
+# Stranger Things Main Theme
+# NOTE: Each measure's total sleep adds up to 4 because the time signature is 4/4
+use_bpm 160
+use_synth :saw
+array=[:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
+index = 0
 
-
+with_fx :distortion do
+  # First set of eighth notes
+  live_loop :stranger_things do
+    8.times do
+      # You put 8.times to play every note in the array
+      play array[index]
+      sleep 0.5
+      index=index+1
+      #Adds to array each time
+    end
+    index=0
+    #allows to start from 0 all over again in loop
+  end
+end
