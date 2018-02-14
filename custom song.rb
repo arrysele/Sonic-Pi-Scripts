8f3165dedebf1b49ea1ab5b00e8f1a6b0d3b9991
arrysele <36054143+arrysele@users.noreply.github.com>
2018-02-14 14:39:42 -0800

    Add files via upload

---
+++ custom song.rb
@@ -0,0 +1,61 @@
+#NAME OF SONG:Havana by Camila Cabello
+#SHEET MUSIC: https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0179610
+#BPM USED:https://songbpm.com/havana
+use_bpm 105
+use_synth :piano
+play :C4
+sleep 0.5
+2.times do
+  play :E4
+  sleep 0.5
+end
+2.times do
+  play :C4
+  sleep 0.5
+end
+play :A3
+sleep 1
+play :r
+sleep 1
+play :r
+sleep 0.5
+live_loop :Havana do
+  play :E4
+  sleep 0.5
+  play :D4
+  sleep 0.5
+  play :E4
+  sleep 0.5
+  play :F4
+  sleep 0.5
+  play :E4
+  sleep 0.5
+  play :D4
+  sleep 0.5
+  play :C4
+  sleep 0.5
+  2.times do
+    play :E4
+    sleep 0.5
+  end
+  2.times do
+    play :C4
+    sleep 0.5
+  end
+  play :A3
+  sleep 1
+  play :r
+  sleep 1
+  play :r
+  sleep 0.5
+end
+
+live_loop :Havana2 do
+  
+  
+  
+  
+  
+  
+  
+end
